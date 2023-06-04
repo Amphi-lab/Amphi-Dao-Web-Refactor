1bf8f8e9cf9bbb266d9fe35cb396da9c0380b85c
echo <heart0magic@163.com>
2023-06-04 17:50:52 +0800

    build: 添加prettier配置

---
+++ .prettierrc.ts
@@ -0,0 +1,13 @@
+module.exports = {
+	tabWidth: 4,
+	useTabs: false,
+	printWidth: 100,
+	semi: true,
+	singleQuote: true,
+	arrowParens: 'avoid',
+	trailingComma: 'all', // 尾随逗号
+	bracketSpacing: true, // 对象中打印空格
+	jsxSingleQuote: true,
+	insertPragma: false, // 插入@format标识
+	ignorePath: '.prettierignore',
+};

8dee4e403c3b176bf7eaf606619abbd5984bb2e5
echo <heart0magic@163.com>
2023-06-04 18:43:10 +0800

    build: 更改prettier配置

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    bracketSpacing: true, // 对象中打印空格
+    jsxSingleQuote: true,
+    ignorePath: '.prettierignore',
+};

55a11fe2c59cc7c0d34c7b2cb06473cd814da7d8
echo <heart0magic@163.com>
2023-06-08 17:09:17 +0800

    refactor: 完善下拉菜单

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,37 +1,15 @@
 import React from 'react';
+import { Dropdown } from 'antd';
 import type { MenuProps } from 'antd';
-import { Button, Dropdown } from 'antd';
 
-const items: MenuProps['items'] = [
-    {
-        key: '1',
-        label: (
-            <a target='_blank' rel='noopener noreferrer' href='https://www.antgroup.com'>
-                1st menu item
-            </a>
-        )
-    },
-    {
-        key: '2',
-        label: (
-            <a target='_blank' rel='noopener noreferrer' href='https://www.aliyun.com'>
-                2nd menu item
-            </a>
-        )
-    },
-    {
-        key: '3',
-        label: (
-            <a target='_blank' rel='noopener noreferrer' href='https://www.luohanacademy.com'>
-                3rd menu item
-            </a>
-        )
-    }
-];
+interface IProps {
+    headName: string;
+    items: MenuProps['items'][];
+}
 
-const AmDropdown: React.FC = () => (
+const AmDropdown: React.FC<IProps> = ({ headName, items }) => (
     <Dropdown menu={{ items }} placement='bottom'>
-        <Button>Head</Button>
+        <span>{headName}</span>
     </Dropdown>
 );
 

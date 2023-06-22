7e4a3d1a43c6ccdbe9a7f6211aef91a29be47a0f
echo <heart0magic@163.com>
2023-06-23 00:22:10 +0800

    feat: 发单页编写

---
+++ src/pages/requestTranslation/Form/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RequestForm = () => {
+    return <div>form</div>;
+};
+
+export default RequestForm;
---
+++ src/pages/requestTranslation/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RequestForm from './Form';
+
+const RequestTranslation = () => {
+    return (
+        <div>
+            {/* <SubBanner subTitle='Get your professional translation' /> */}
+            <RequestForm />
+        </div>
+    );
+};
+
+export default RequestTranslation;

0e271afdc04d1ee099d830af300d6663c07207f5
echo <heart0magic@163.com>
2023-06-28 19:38:43 +0800

    feat: 订单详情页组件初始化

---
+++ src/pages/orderDetail/TranCandidate/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TranCandidate = () => {
+    return <div>TranCandidate</div>;
+};
+
+export default TranCandidate;
---
+++ src/pages/orderDetail/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Schedule from './Schedule';
+import Detail from './Detail';
+import TranCandidate from './TranCandidate';
+
+const OrderDetail = () => {
+    return (
+        <div>
+            <Schedule />
+            <Detail />
+            <TranCandidate />
+        </div>
+    );
+};
+
+export default OrderDetail;

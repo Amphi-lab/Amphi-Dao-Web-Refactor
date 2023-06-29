dc30ab3a874aae9577689abd62adbd0d801983bc
shixm1102 <shixm_1102@qq.com>
2023-06-30 02:25:35 +0800

    fix: getSubStr

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,5 +1,6 @@
 // 字符串过长，中间打点
 export const getSubStr = (str: string): string => {
+    if (typeof str !== 'string') return '';
     if (str.length < 10) return str;
     return `${str.slice(0, 4)}...${str.slice(-4)}`;
 };

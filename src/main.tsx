e653d24c72ddc83e63c8a8191813141615d68106
shixm1102 <shixm_1102@qq.com>
2023-07-04 00:38:19 +0800

    fix: 改为hash路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { WagmiConfig } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';

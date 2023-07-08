851a9729abeb2742ae4502ca0bc6b46f4ac465be
echo <heart0magic@163.com>
2023-07-08 11:31:26 +0800

    perf: 优化取消任务对话框样式

--- src/pages/orderDetail/Detail/index.tsx
+++ src/pages/orderDetail/Detail/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AmCard from '@/components/Card';
 import IconButton from '@/components/IconButton';
 import EditIcon from '@/components/Icon/Edit';
 import CancelIcon from '@/components/Icon/Cancel';
 import { ExclamationCircleFilled } from '@ant-design/icons';
-import { Modal } from 'antd';
+import { Button, Modal } from 'antd';
 import { useNavigate } from 'react-router';
 // import { useLocation } from 'react-router-dom';
+// import { getAmphi } from '@/contracts/contract';
+// import { useAppSelector } from '@/store/hooks';
+// import { translationIndex } from '@/store/reducers/orderDetailSlice';
 import OrderDes from './orderDes';
+import styles from './index.module.scss';
 
 const { confirm } = Modal;
 
@@ -20,21 +24,64 @@ const titleStyle = {
 };
 
 const Detail = () => {
+    // const transIndex = useAppSelector(translationIndex);
+    const navigate = useNavigate();
     // const location = useLocation();
     // const id = location.state || +location.pathname.split('/')[2];
-    const navigate = useNavigate();
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    // 取消任务
+    const cancelTask = async () => {
+        // const amphi = await getAmphi();
+        // amphi.methods
+        //     .closeTask(transIndex)
+        //     .call()
+        //     .then((data: any) => {
+        //         console.log(data);
+        //     })
+        //     .catch((err: any) => {
+        //         console.log('err', err);
+        //     });
+    };
+
+    const handleOk = () => {
+        console.log('ok', isOpen);
+        cancelTask();
+        setIsOpen(false);
+    };
+
+    const handleCancel = () => {
+        console.log('cancelled', isOpen);
+        setIsOpen(false);
+    };
 
     const showConfirm = () => {
         confirm({
+            open: isOpen,
             title: 'Warning!',
             icon: <ExclamationCircleFilled />,
             content: 'Are you sure you want to cancel?',
-            onOk() {
-                console.log('OK');
-            },
-            onCancel() {
-                console.log('Cancel');
-            }
+            closable: true,
+            maskClosable: true,
+            // okText: 'No',
+            // cancelText: 'Yes',
+            // onOk() {
+            //     console.log('OK');
+            // },
+            // onCancel() {
+            //     console.log('Cancel');
+            // },
+            footer: (
+                <div className={styles['custom-cancal-modal']}>
+                    <Button className={styles['yes-btn']} onClick={handleOk}>
+                        Yes
+                    </Button>
+                    <Button className={styles['no-btn']} type='primary' onClick={handleCancel}>
+                        No
+                    </Button>
+                </div>
+            )
         });
     };
 

d2647a628461cb7419070e58ead224aa6f3b6522
echo <heart0magic@163.com>
2023-07-09 21:44:33 +0800

    revert: 放开orderDetai组件控制

--- src/pages/orderDetail/[id].tsx
+++ src/pages/orderDetail/[id].tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useAppSelector } from '@/store/hooks';
-import { currentStep } from '@/store/reducers/orderDetailSlice';
+// import { useAppSelector } from '@/store/hooks';
+// import { currentStep } from '@/store/reducers/orderDetailSlice';
 import Schedule from './Schedule';
 import Detail from './Detail';
 import TranCandidate from './TranCandidate';
@@ -11,18 +11,19 @@ import ServiceRating from './ServiceRating';
 // import routes from '~react-pages';
 
 const OrderDetail = () => {
-    const step = useAppSelector(currentStep);
+    // const step = useAppSelector(currentStep);
     return (
         <div className={styles['order-detail-wrapper']}>
             <Schedule />
             <Detail />
-            {step === 1 && <TranCandidate />}
+            {/* {step === 1 && <TranCandidate />}
             {(step === 2 || step === 3) && <TranContent />}
             {(step === 2 || step === 3) && <Notification />}
-            {step === 3 && <ServiceRating />}
-            {/* <TranContent />
+            {step === 3 && <ServiceRating />} */}
+            <TranCandidate />
+            <TranContent />
             <Notification />
-            <ServiceRating /> */}
+            <ServiceRating />
         </div>
     );
 };

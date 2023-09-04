bc46116cd96ac36afdfac6d86355875a149e81cf
whybfq <13588119112@163.com>
2023-09-04 08:02:09 +0800

    feat(connect wallet): add all reviews and judge

---
+++ src/pages/allreviews/index.tsx
@@ -0,0 +1,108 @@
+import React, { useCallback, useMemo } from 'react';
+import { Select, Tabs } from 'antd';
+import dayjs from 'dayjs';
+
+import api from '@/api';
+import {
+    acceptAddressColumn,
+    actionColumn,
+    bountyColumn,
+    deadlineColumn,
+    titleColumn,
+    translateTypeColumn,
+    translationStateColumn,
+    translationTypeArrayFormItem
+} from '@/pageComponents/myorders/TabsItems';
+import OrderLayout, { createBaseTabItems } from '@/layout/OrderLayout';
+import { useAccount } from 'wagmi';
+
+const { Option } = Select;
+
+const formatType = 'YYYY-MM-DD HH:mm:ss'; // HH:mm:ss
+
+export default () => {
+    const { address } = useAccount();
+
+    const currentDate = dayjs().format(formatType);
+    const year = dayjs().year();
+    const yearRange = JSON.stringify([
+        dayjs().startOf('year').format(formatType),
+        dayjs().endOf('year').format(formatType)
+    ]);
+    const monthRange = JSON.stringify([
+        dayjs().subtract(3, 'month').startOf('day').format(formatType),
+        currentDate
+    ]);
+    const dayRange = JSON.stringify([
+        dayjs().subtract(30, 'day').startOf('day').format(formatType),
+        currentDate
+    ]);
+
+    const queryItems = useMemo(() => {
+        return [
+            translationTypeArrayFormItem,
+            {
+                name: 'createTime',
+                className: 'filter-create-time',
+                label: 'Created at',
+                itemRender: (
+                    <Select placeholder='Select a option and change input text above'>
+                        <Option value={dayRange}>last 30 days</Option>
+                        <Option value={monthRange}>past 3 months</Option>
+                        <Option value={yearRange}>{year}</Option>
+                        <Option value=''>all time periods</Option>
+                    </Select>
+                )
+            }
+        ];
+    }, [dayRange, monthRange, year, yearRange]);
+
+    const columns = useMemo(() => {
+        return [
+            titleColumn,
+            acceptAddressColumn,
+            bountyColumn,
+            translationStateColumn,
+            deadlineColumn,
+            translateTypeColumn,
+            actionColumn
+        ];
+    }, []);
+
+    const getTranslationList = useCallback((queryParams: any) => {
+        return api.getTranslationList(queryParams).then((res: any) => {
+            if (res.code === 200) {
+                const { rows, total } = res;
+                return { rows, total };
+            }
+        });
+    }, []);
+    const formatQueryParams = useCallback(
+        (formValues: any, status?: string) => {
+            const { translationTypeArray, createTime } = formValues;
+            const options: any = {
+                translationStateArray: status?.split(','),
+                buyerAddress: address
+            };
+            if (translationTypeArray.length > 0) {
+                options.translationTypeArray = translationTypeArray.join();
+            }
+            const [beginCreateTime, endCreateTime] = createTime ? JSON.parse(createTime) : [];
+            if (beginCreateTime) options.params = { ...options.params, beginCreateTime };
+            if (endCreateTime) options.params = { ...options.params, endCreateTime };
+
+            return options;
+        },
+        [address]
+    );
+
+    const items = useMemo(() => {
+        return createBaseTabItems(columns, getTranslationList, formatQueryParams, queryItems);
+    }, [columns, getTranslationList, formatQueryParams, queryItems]);
+
+    return (
+        <OrderLayout title='Review' tabsItems={items}>
+            <Tabs defaultActiveKey='1' items={items} />
+        </OrderLayout>
+    );
+};

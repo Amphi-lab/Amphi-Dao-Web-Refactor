4a0ea9716b8ab1d738dff75df36bca75aad84ff0
echo <heart0magic@163.com>
2023-09-15 20:13:48 +0800

    feat: 部署接口

--- src/api/interface/0915.ts
+++ src/api/interface/0915.ts
@@ -12,4 +12,9 @@ export const addGroup = (id:any) => get(`/competition/doJoin/${id}`);
 // 获取任务列表  /competition/getTaskList
 export const getTaskList = () => get(`/competition/getTaskList`);
 
+// 申请任务  /competition/apply/{taskId}
+export const getApplyTask = (taskId:string) => get(`/competition/apply/${taskId}`);
+
+// 获取已经 apply的任务 /competition/getAppliedTaskList
+export const getAppliedTaskList = get(`/competition/getAppliedTaskList`);
 
